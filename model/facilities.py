574c9b66af8e4fd0f8f102acadbb5b31df334d59
tanjazoejane <tanja.luescher@hotmail.com>
2025-05-25 19:29:08 +0200

    facilities getter / setter korrigiert, autoincrement eingestellt

--- model/facilities.py
+++ model/facilities.py
@@ -1,26 +1,23 @@
 class Facilities:
 
-    def __init__(
-            self,
-            facility_id : int, #Schreibfehler
-            facility_name : str,
-            ):
-    
-        self.__facility_id : int = facility_id
-        self.__facility_name : str = facility_name
+    def __init__(self, facility_id : int, facility_name : str):
+
+        if not facility_name:
+            raise ValueError ("facility_name fehlt")
+        if not isinstance(facility_name , str):
+             raise TypeError("facility_name muss ein string sein")
+
+        self.__facility_id = facility_id
+        self.__facility_name = facility_name
 
     @property
-    def facility_id(self) -> int: #Schreibfehler
+    def facility_id(self):
         return self.__facility_id
 
-
     @property
-    def facility_name(self) -> str:
+    def facility_name(self):
         return self.__facility_name
 
     @facility_name.setter
-    def facility_name(self, facility_name : str):
-        if not facility_name:
-            raise ValueError ("facility_name required")
-        if not isinstance(facility_name , str):
-             raise TypeError("facility_name must be a string")
+    def facility_name(self, facility_name):
+        self.__facility_name = facility_name
