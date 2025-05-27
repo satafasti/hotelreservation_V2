058bdf06152e01e5a834fce06e4ee9fe58ee0781
tanjazoejane <tanja.luescher@hotmail.com>
2025-05-27 21:33:32 +0200

    facilities manager erstellt und im init neue manager klassen auskommentierung gelöscht

--- business_logic/__init__.py
+++ business_logic/__init__.py
@@ -1,8 +1,8 @@
 from business_logic.booking_manager import BookingManager
 from .facilities_manager import FacilitiesManager
-#from .guest_manager import GuestManager #wurde noch nicht erstellt
+from .guest_manager import GuestManager
 from .hotel_manager import HotelManager
-#from .invoice_manager import InvoiceManager #wurde noch nicht erstellt
-#from .room_facilities_manager import #Benötigen wir das? ist doch nur Zwischentable in SQL?
+from .invoice_manager import InvoiceManager
+from .room_facilities_manager import RoomFacilitiesManager
 from .room_manager import RoomManager
 from .room_type_manager import RoomTypeManager
---
+++ business_logic/facilities_manager.py
@@ -0,0 +1,34 @@
+from typing import Optional
+from data_access.facilities_dal import FacilityDataAccess
+import model
+
+
+class FacilitiesManager:
+    def __init__(self, facilities_dal: FacilityDataAccess):
+        self._dal = facilities_dal
+
+    def create_facility(self, facility_id: int, facility_name: str) -> model.Facilities:
+        if not facility_name:
+            raise ValueError("Facility name wird benötigt")
+        return self._dal.create_new_facility(facility_id, facility_name)
+
+    def get_facility_by_id(self, facility_id: int) -> Optional[model.Facilities]:
+        return self._dal.read_facility_by_id(facility_id)
+
+    def get_all_facilities(self) -> list[model.Facilities]:
+        return self._dal.read_all_facilities()
+
+    def update_facility(self, facility_id: int, facility_name: str):
+        if not facility_name:
+            raise ValueError("Facility name wird benötigt")
+        self._dal.update_facility(facility_id, facility_name)
+
+    def delete_facility(self, facility_id: int):
+        self._dal.delete_facility(facility_id)
+
+    def facility_exists(self, facility_id: int) -> bool:
+        return self._dal.read_facility_by_id(facility_id) is not None
+
+    def is_name_unique(self, facility_name: str) -> bool:
+        all_facilities = self.get_all_facilities()
+        return all(f.facility_name != facility_name for f in all_facilities)
