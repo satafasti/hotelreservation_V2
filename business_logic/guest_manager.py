f3a26062ce50a6bb23416b145dc7243a46fc1641
Fabia Holzer <holzerpamina@gmail.com>
2025-05-27 18:41:29 +0200

    Business Logic Guest Manager vervollständigt

---
+++ business_logic/guest_manager.py
@@ -0,0 +1,24 @@
+from data_access.guest_dal import GuestDAL
+from model.guest import Guest
+from typing import Optional, List
+
+
+class GuestManager:
+    def __init__(self, db_path: str = None):
+        self.__dal = GuestDAL(db_path)
+
+    def create_guest(self, first_name: str, last_name: str, email: str, address_id: int) -> Guest:
+        guest = Guest(None, first_name, last_name, email, address_id)
+        return self.__dal.create_guest(guest)
+
+    def get_guest_by_id(self, guest_id: int) -> Optional[Guest]:
+              return self.__dal.read_guest_by_id(guest_id)
+
+    def get_all_guests(self) -> List[Guest]:
+              return self.__dal.read_all_guests()
+
+    def update_guest(self, guest: Guest):
+             self.__dal.update_guest(guest)
+
+    def delete_guest(self, guest: Guest):
+                self.__dal.delete_guest(guest)
