5cc8611373d88de6bde74be2da9ce1135a8840da
grabs2 <sarinag_official@hotmail.com>
2025-05-27 19:31:08 +0200

    class Room gem. Referenzprojekt erstellt

--- model/room.py
+++ model/room.py
@@ -1,79 +1,92 @@
 from __future__ import annotations
-#from typing import TYPE_CHECKING #Referenzprojekt
-import model.room_type
-import model.facilities
-#from Sarina_Test import Room_Type #=> wird noch benötigt?
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from model.hotel import Hotel
+    from model.room_type import Room_Type
+    from model.room_facilities import Room_Facilities
 
-#if TYPE_CHECKING: #Referenzprojekt
-    #from model.artist import Artist #Referenzprojekt
-    #from model.track import Track #Referenzprojekt
-
+#TODO Code für Projekt ergänzen
+### Code gemäss Referenzprojekt
 class Room:
-    def __init__(self, room_id : int, room_number : int, price_per_night : float,  description: model.room_type.Room_Type, room_available=True): #ID ist in SQL enthalten?
+    def __init__(self, room_id: int, hotel: Hotel, room_number: str, room_type: Room_Type, price_per_night: float):
         if not room_id:
-            raise ValueError("room_id must be set")
+            raise ValueError("room_id wird benötigt.") #wird das benötigt? sollte eigentlich automatisch erstellt werden durch DB.
         if not isinstance(room_id, int):
-            raise TypeError("room_id must be a int")
+            raise ValueError("room_id muss integer sein.")
         if not room_number:
-            raise ValueError("room_number must be set")
-        if not isinstance(room_id, int):
-            raise TypeError("room_number must be a int")
+            raise ValueError("room_number wird benötigt.")
+        if not isinstance(room_number, str):
+            raise ValueError("room_number muss string sein.")
         if not price_per_night:
-            raise ValueError("price_per_night must be set")
+            raise ValueError("price_per_night wird benötigt.")
         if not isinstance(price_per_night, float):
-            raise TypeError("price_per_night must be a float")
+            raise ValueError("price_per_night muss float sein.")
 
-        self.__room_id = room_id
-        self.__room_number = room_number
-        self.__price_per_night = price_per_night
-        self.__description = description #Description von Class room_type soll übergeben werden
-        self.__facility_name = [] #Liste für Facilities des Raumes
-        self.room_available = room_available
+        self.__room_id: int = room_id
+        self.__room_number: str = room_number
+        self.__hotel: Hotel = hotel
+        if hotel is not None:
+            hotel.add_room(self)
+        self.__room_facilities: list[Room_Facilities] = [] #TODO Room_Facilities erstellen
 
+    def __repr__(self):
+        return f"Room(id={self.__room_id!r}, room_number={self.__room_number!r}, hotel={self.__hotel!r})"
 
     @property
-    def room_id(self):
+    def room_id(self) -> int:
         return self.__room_id
 
-    @room_id.setter
-    def room_id(self,new_room_id):
-        self.__room_id = new_room_id
-
-#room_number
     @property
-    def room_number(self):
+    def room_number(self) -> str:
         return self.__room_number
 
     @room_number.setter
-    def room_number(self,new_room_number):
-        self.__room_number = new_room_number
+    def room_number(self, room_number: str) -> None:
+        if not room_number:
+            raise ValueError("room_number wird benötigt.")
+        if not isinstance(room_number, str):
+            raise ValueError("room_number muss string sein.")
+        self.__room_number = room_number
 
-#price per night
     @property
-    def price_per_night(self):
-        return self.__price_per_night
+    def hotel(self) -> Hotel:
+        return self.__hotel
 
-    @price_per_night.setter
-    def price_per_night(self, new_price_per_night):
-        self.__price_per_night = new_price_per_night
+    @hotel.setter
+    def hotel(self, hotel: Hotel) -> None:
+        from model import Hotel
+        if hotel is not None and not isinstance(hotel, Hotel):
+            raise ValueError("hotel muss Bestandteil von Hotel sein.")
+        if self.__hotel is not hotel:
+            if self.__hotel is not None:
+                self.__hotel.remove_room(self)
+            self.__hotel = hotel
+            if hotel is not None and self not in hotel.room:
+                hotel.add_room(self)
 
-
-#description
     @property
-    def description(self):
-        return self.__description
+    def room_facilities(self) -> list[Room_Facilities]:
+        return self.__room_facilities.copy()
 
-    @description.setter
-    def description(self,new_description):
-        self.__description = new_description
+    def add_room_facilities(self, room_facilities: Room_Facilities) -> None:
+        from model import Room_Facilities
 
+        if not room_facilities:
+            raise ValueError("room_facilities wird benötigt.")
+        if not isinstance(room_facilities, Room_Facilities):
+            raise ValueError("room_facilities muss Bestandteil von Room_Facilities sein.")
+        if room_facilities not in self.__room_facilities:
+            self.__room_facilities.append(room_facilities)
+            room_facilities.room = self
 
-#facility_name
-    @property
-    def facility_name(self):
-        return self.__facility_name
+    def remove_room_facilities(self, room_facilities: Room_Facilities) -> None:
+        from model import Room_Facilities
 
-    @facility_name.setter
-    def facility_name(self,new_facility_name):
-        self.__facility_name = new_facility_name
+        if not room_facilities:
+            raise ValueError("room_facilities wird benötigt.")
+        if not isinstance(room_facilities, Room_Facilities):
+            raise ValueError("room_facilities muss Bestandteil von Room_Facilities sein.")
+        if room_facilities in self.__room_facilities:
+            self.__room_facilities.remove(room_facilities)
+            room_facilities.room = None
