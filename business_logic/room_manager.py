ce4c1d094c93b5be10f1eb0d12396e2ed5599a87
st1rl1ng <stirling.mulholland@students.fhnw.ch>
2025-05-29 22:07:12 +0200

    room manager read hotel room

--- business_logic/room_manager.py
+++ business_logic/room_manager.py
@@ -11,7 +11,7 @@ class RoomManager():
     def create_room(self, hotel_id: int, room_number: str, type_id: int, price_per_night: float, hotel: model.Hotel = None) -> model.Room:
         return self.__room_dal.create_room(hotel_id, room_number, type_id, price_per_night, hotel)
 
-    def read_hotels_rooms(self, Hotel.name : str) -> None:
+    def read_hotels_rooms(self, hotel: model.Hotel) -> None:
         return self.__room_dal.read_rooms_by_hotel(hotel)
 
     def read_room(self, room_id: int) -> model.Room:
