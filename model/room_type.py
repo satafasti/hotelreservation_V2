271c376711212083e5624e8697c8914f2c9d21cb
grabs2 <sarinag_official@hotmail.com>
2025-05-29 19:23:40 +0200

    übersetzung

--- model/room_type.py
+++ model/room_type.py
@@ -2,17 +2,17 @@ class Room_Type:
 
     def __init__(self, type_id : int, description : str, max_guests : int):
         if not type_id:
-            raise ValueError("type_id must be set")
+            raise ValueError("type_id wird benötigt.")
         if not isinstance(type_id, int):
-            raise TypeError("type_id must be a int")
+            raise TypeError("type_id muss ein integer sein.")
         if not description:
-            raise ValueError("Description is required")
+            raise ValueError("Description wird benötigt.")
         if not isinstance(description, str):
-            raise TypeError("Description must be a string")
+            raise TypeError("Description muss string sein.")
         if not max_guests:
-            raise ValueError("max_guests is required")
+            raise ValueError("max_guests wird benötigt.")
         if not isinstance(max_guests, int):
-            raise TypeError("max_guests must be an integer")
+            raise TypeError("max_guests muss integer sein.")
 
         self.__type_id = type_id
         self.__description = description #z.B. Einzelzimmer
