b748dfdeaa17be2915a86758373850cb6b44e491
grabs2 <sarinag_official@hotmail.com>
2025-05-29 18:58:30 +0200

    Room_Type type_id aktiviert

--- model/room_type.py
+++ model/room_type.py
@@ -1,10 +1,10 @@
 class Room_Type:
 
     def __init__(self, type_id : int, description : str, max_guests : int):
-        #if not type_id:
-            #raise ValueError("type_id must be set")
-        #if not isinstance(type_id, int):
-           # raise TypeError("type_id must be a int") wird durch autoincrement gesetzt und kann daher nur int sein
+        if not type_id:
+            raise ValueError("type_id must be set")
+        if not isinstance(type_id, int):
+            raise TypeError("type_id must be a int")
         if not description:
             raise ValueError("Description is required")
         if not isinstance(description, str):
@@ -22,13 +22,13 @@ class Room_Type:
     def type_id(self):
         return self.__type_id
 
-    #@room_type_id.setter => Da Autoincrementation von SQL
-    #def type_id(self,new_type_id):
-        #self.__type_id = new_type_id
+    @room_type_id.setter
+    def type_id(self,new_type_id):
+        self.__type_id = new_type_id
 
-    #@type_id.deleter #sollte nicht benötigt werden, da id alleine eigentlich nie gelöscht werden muss
-    #def type_id(self):
-        #del self.__type_id
+    @type_id.deleter #sollte nicht benötigt werden, da id alleine eigentlich nie gelöscht werden muss
+    def type_id(self):
+        del self.__type_id
 
     @property
     def description(self):
