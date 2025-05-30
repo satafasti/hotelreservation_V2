e9f441fb72d1b270045dce0615d2e3876bba12e5
Sarina <154918073+grabs2@users.noreply.github.com>
2025-05-30 16:27:58 +0200

    read_room_details ergänzt

--- business_logic/room_manager.py
+++ business_logic/room_manager.py
@@ -6,7 +6,7 @@ import data_access
 ### Code gemäss Referenzprojekt
 class RoomManager():
     def __init__(self) -> None:
-        self.__room_dal = data_access.RoomDataccess()
+        self.__room_dal = data_access.RoomDataAccess()
 
     def create_room(self, hotel_id: int, room_number: str, type_id: int, price_per_night: float, hotel: model.Hotel = None) -> model.Room:
         return self.__room_dal.create_room(hotel_id, room_number, type_id, price_per_night, hotel)
@@ -16,3 +16,6 @@ class RoomManager():
 
     def read_room(self, room_id: int) -> model.Room:
         return self.__room_dal.read_room_by_id(room_id)
+
+    def read_room_details(self, model: model.Room) -> model.Room:
+        return self.__room_dal.
