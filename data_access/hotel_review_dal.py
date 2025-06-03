fb510a6295a955435f8459e9b11c37dbfda92d71
Sarina <154918073+grabs2@users.noreply.github.com>
2025-06-03 14:33:11 +0200

    create_hotel_review und read_reviews_by_hotel_id erstellt

--- data_access/hotel_review_dal.py
+++ data_access/hotel_review_dal.py
@@ -1,8 +1,34 @@
+from tkinter.constants import INSERT
+
 import model
 from data_access.base_dal import BaseDataAccess
+from model.hotel_review import HotelReview
+
 
 class HotelReviewDataAccess(BaseDataAccess):
     def __init__(self, db_path: str = None):
         super().__init__(db_path)
 
-    def create_new_hotel(self, hotel: m
+    def create_hotel_review(self, hotel_review: HotelReview):
+        if hotel_review is None:
+            raise ValueError("Hotel Review wird benötigt.")
+
+        sql = """
+              INSERT INTO Hotel_Review (guest_id, hotel_id, booking_id, rating, comment, review_date)
+              VALUES (?, ?, ?, ?, ?, ?)
+              """
+        params = (hotel_review.guest_id, hotel_review.hotel_id, hotel_review.booking_id, hotel_review.rating, hotel_review.comment, hotel_review.review_date)
+
+        last_row_id, _ = self.execute(sql, params)
+        hotel_review.review_id = last_row_id
+        return hotel_review
+
+    def read_reviews_by_hotel_id(self, hotel_id: int) -> list[HotelReview]:
+        sql = """
+              SELECT review_id, guest_id, hotel_id, booking_id, rating, comment, review_date
+              FROM Hotel_Review
+              WHERE hotel_id = ?
+              ORDER BY review_date DESC 
+              """
+        results = self.fetchall(sql, (hotel_id,))
+        return [HotelReview(*row) for row in results]
