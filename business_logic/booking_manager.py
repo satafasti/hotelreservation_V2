030c5e13b5aa80aec02ee69fd6e3e537459ff387
Fabia Holzer <holzerpamina@gmail.com>
2025-06-07 14:49:48 +0200

    month hinzugefügt

--- business_logic/booking_manager.py
+++ business_logic/booking_manager.py
@@ -13,7 +13,7 @@ class BookingManager:
         check_in = datetime.strptime(check_in_date, "%Y-%m-%d")
         check_out = datetime.strptime(check_out_date, "%Y-%m-%d")
         num_nights = (check_out - check_in).days
-        dynamic_price = self.calculate_dynamic_price(price_per_night, check_in_date)
+        dynamic_price = self.calculate_dynamic_price(price_per_night, check_in)
         total_price = num_nights * dynamic_price
 
         booking = Booking(
@@ -29,7 +29,11 @@ class BookingManager:
         return self.__dal.create_booking(booking)
 
     def calculate_dynamic_price(self, base_price: float, check_in: str) -> float:
-        month = check_in.month
+        if isinstance(check_in, str):
+            check_in_date = datetime.strptime(check_in, "%Y-%m-%d")
+        else:
+            check_in_date = check_in
+        month = check_in_date.month
         if month in [6, 7, 8, 12]:
             return base_price * 1.3  # Hochsaison
         elif month in [1, 2, 3, 11]:
