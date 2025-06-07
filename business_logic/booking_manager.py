2aeeaf3ca46c475c1430a6d580a0bc5ea5a3b765
Fabia Holzer <holzerpamina@gmail.com>
2025-06-07 12:44:36 +0200

    check-in.month angepasst bei Booking Manager

--- business_logic/booking_manager.py
+++ business_logic/booking_manager.py
@@ -29,7 +29,8 @@ class BookingManager:
         return self.__dal.create_booking(booking)
 
     def calculate_dynamic_price(self, base_price: float, check_in: str) -> float:
-        month = check_in.month
+        check_in_date = datetime.strptime(check_in, "%Y-%m-%d")
+        month = check_in_date.month
         if month in [6, 7, 8, 12]:
             return base_price * 1.3  # Hochsaison
         elif month in [1, 2, 3, 11]:
