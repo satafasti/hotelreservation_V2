64f3e2d0e8df86d6d500881f1d3263301ceb5fc2
Sarina <154918073+grabs2@users.noreply.github.com>
2025-06-11 14:24:45 +0200

    get_all_facilities, add_facilities_to_first_room ergänzt und auskommentierte Funktionen aktiviert

--- business_logic/hotel_manager.py
+++ business_logic/hotel_manager.py
@@ -1,9 +1,12 @@
 import model
 from data_access.hotel_dal import HotelDataAccess
+from data_access.room_facilities_dal import RoomFacilitiesDataAccess  # Neu hinzufügen
+
 
 class HotelManager:
     def __init__(self, db_path: str = None):
         self.__hotel_dal = HotelDataAccess(db_path)
+        self.__facilities_dal = RoomFacilitiesDataAccess(db_path)
 
     def create_hotel(self, hotel: model.Hotel, address: model.Address, room: model.Room):
         return self.__hotel_dal.create_new_hotel(hotel, address, room)
@@ -20,15 +23,38 @@ class HotelManager:
     def add_room_to_hotel(self, hotel_id, room_number, description, max_guests, price_per_night, selected_facility_ids):
         return self.__hotel_dal.add_room_to_hotel(hotel_id, room_number, description, max_guests, price_per_night, selected_facility_ids)
 
+    def get_all_facilities(self):
+        return self.__facilities_dal.read_all_facilities()
+
+    def add_facilities_to_first_room(self, hotel_id, selected_facility_ids):
+        if not selected_facility_ids:
+            return 0
+
+        room_id_sql = "SELECT room_id FROM Room WHERE hotel_id = ? ORDER BY room_id LIMIT 1"
+        room_result = self.__facilities_dal.fetchone(room_id_sql, (hotel_id,))
+
+        if room_result:
+            first_room_id = room_result[0]
+
+            for facility_id in selected_facility_ids:
+                facility_insert_sql = "INSERT INTO Room_Facilities (room_id, facility_id) VALUES (?, ?)"
+                self.__facilities_dal.execute(facility_insert_sql, (first_room_id, facility_id))
+
+            return len(selected_facility_ids)
+        return 0
+
+    def read_hotel(self, hotel_id: int):
+        return self.__hotel_dal.read_hotel_by_id(hotel_id)
+
+    def read_all_hotels(self) -> list[model.Hotel]:
+        return self.__hotel_dal.read_all_hotels()
+
+    def update_hotel(self, hotel: model.Hotel) -> None:
+        self.__hotel_dal.update_hotel(hotel)
+
 
 #Das Projekt enthält im Modul HotelManager Methoden wie read_hotel, read_all_hotels oder update_hotel. Diese Methoden leiten im Wesentlichen nur an die Data‑Access‑Schicht (HotelDataAccess) weiter.
 #Im User Interface wird jedoch direkt HotelDataAccess verwendet. So werden Hotels z.B. im Gast-UI eingelesen und ausgewählt,
-
-    #def read_hotel(self, hotel_id: int):
-        #return self.__hotel_dal.read_hotel_by_id(hotel_id)
-
-    #def read_all_hotels(self) -> list[model.Hotel]:
-        #return self.__hotel_dal.read_all_hotels()
 
     # def read_all_hotels_as_df(self) -> pd.DataFrame:
         #return self.__hotel_dal.read_all_hotels_as_df()
@@ -36,5 +62,3 @@ class HotelManager:
     # def read_hotels_by_similar_name_as_df(self, name: str) -> pd.DataFrame:
         #return self.__hotel_dal.read_hotels_like_name_as_df(name)
 
-    #def update_hotel(self, hotel: model.Hotel) -> None:
-        #self.__hotel_dal.update_hotel(hotel)
