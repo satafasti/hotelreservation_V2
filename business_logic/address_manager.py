5ba53dc57bb28bd8764c4a86aeb69ce2df347ca4
Fabia Holzer <holzerpamina@gmail.com>
2025-06-13 10:17:30 +0200

    address_manager.py ergänzt mit read_address_by_id und update_address

--- business_logic/address_manager.py
+++ business_logic/address_manager.py
@@ -8,3 +8,9 @@ class AddressManager:
     def create_address(self, address: Address):
         self.dal.create_new_address(address)
         return address
+
+    def read_address_by_id(self, address_id: int) -> Address | None:
+        return self.dal.read_address_by_id(address_id)
+
+    def update_address(self, address: Address) -> None:
+        self.dal.update_address(address)
